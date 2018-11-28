a6f9cb1f1bec9248dde75cd6db464e58c1a4ccc5
Onno Gabriel <contact@datacodedesign.de>
2018-11-28 13:28:33 +0100

    Removed console.log

--- assets/js/wp-visitorflow-frontend.js
+++ assets/js/wp-visitorflow-frontend.js
@@ -2,7 +2,7 @@
 	var wpvfURL = document.getElementById('wpvf-info').getAttribute('data-home-url');
 	wpvfURL += '?wpvf_referrer=' + encodeURIComponent(document.referrer);
 	wpvfURL += '&wpvf_page=' + encodeURIComponent(document.documentURI);
-console.log(wpvfURL)
+
 	var wpvfRequest = new XMLHttpRequest();
 	wpvfRequest.open('GET',wpvfURL);
 	wpvfRequest.send(null);
